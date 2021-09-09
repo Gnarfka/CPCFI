9fb5662b85e576534211afdb32887233c52669c2
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2021-09-09 17:02:21 -0500

    Iniciando Increasing Array

---
+++ Increasing Array/IncreasingArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, j;
+    int flag;
+
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        int aux;
+        cin >> aux
+        arr[i] = aux;
+    }
+
+    while(flag){
+        if(arr[j])
+    }
+
+    return 0;
+}
