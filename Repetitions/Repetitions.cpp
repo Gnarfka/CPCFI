e97f7589a40fe3f610a1d3988e73fbd0b82716ea
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2021-09-09 15:05:29 -0500

    Terminado Repetitions

--- Repetitions/Repetitions.cpp
+++ Repetitions/Repetitions.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main(){
+	string DNA;
+	int cont = 1, i, aux;
 
+	cin>> DNA;
+	for(i=0; i<DNA.length(); i++){
+		if(DNA[i]==DNA[i+1]){
+			cont++;
+		}
+		if(DNA[i]!=DNA[i+1]){
+			cont = 1;
+		}
+		if(cont>=aux){
+				aux = con;
+		}
+	}
 
-    return 0;
+	cout << aux;
+	return 0;
 }
