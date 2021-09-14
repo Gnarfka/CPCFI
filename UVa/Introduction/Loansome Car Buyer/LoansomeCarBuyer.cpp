4ef2a341f7b5201aab7da5c5e02b22ca38934ff1
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2021-09-14 10:17:18 -0500

    Agregados PDF's de los problemas UVa

---
+++ UVa/Introduction/Loansome Car Buyer/LoansomeCarBuyer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+	int month, depreciation;
+	double payment, loan, worth;
+
+	cin >> month >> payment >> loan >> depreciation;
+	worth = loan+payment;
+
+	while(month > 0){
+		cin 
+	}
+
+	return 0;
+}
