eba8b42d3b79eb4f1001f293fd2d2177ae438cd1
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2021-09-14 13:46:23 -0500

    Terminado Loansome Car Buyer

--- UVa/Introduction/Loansome Car Buyer/LoansomeCarBuyer.cpp
+++ UVa/Introduction/Loansome Car Buyer/LoansomeCarBuyer.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 
-int main(){
-	int month, depreciation;
-	double payment, loan, worth;
+using namespace std;
 
-	cin >> month >> payment >> loan >> depreciation;
-	worth = loan+payment;
+int main() {
+	int months, depNumber;
+	double payment, loan;
 
-	while(month > 0){
-		cin 
+	while (true) {
+		cin >> months >> payment >> loan >> depNumber;
+		if (months < 0){
+			break;
+		}
+
+		double deps[months];
+
+		int m;
+		double p;
+		while (depNumber--) {
+			cin >> m >> p;
+			for (int i = m; i < months; i++)
+				deps[i] = p;
+		}
+
+		int month = 0;
+		double monthlyPay = loan / months;
+		double worth = (loan + payment) * (1 - deps[0]);
+		double curLoan = loan;
+		while (worth < curLoan) {
+			month++;
+			curLoan -= monthlyPay;
+			worth = worth * (1-deps[month]);
+		}
+
+		cout << month << " month";
+		if (month != 1){
+			cout << "s";
+		}
+		cout << endl;
 	}
 
 	return 0;
