6871a8cd1eb0fd9b2c696dde82d1e90e32484ffc
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2021-09-18 11:36:40 -0500

    Terminado Beat the Spread

---
+++ UVa/Introduction/Beat the Spread/BeatSpread.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	long long int n, s, d, x, y;
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++){
+		cin >> s >> d;
+
+		if((s-d) < 0 || (s+d)%2!=0 || (s-d)%2!=0){
+			cout << "impossible" << endl;
+		}
+		else{
+			x = (s+d)/2;
+			y = (s-d)/2;
+			if(x > y){
+				cout << x << " " << y << endl;
+			}
+			else{
+				cout << y << " " << abs(x) << endl;
+			}
+		}		
+	}
+
+	return 0;
+}
